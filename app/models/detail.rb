2657fbab07bbffd5fab1a22507ac10e49d4c906e
Yigit Mersin <yigitmersin@gmail.com>
2022-05-03 19:25:12 +0300

    Add detail model test

--- app/models/detail.rb
+++ app/models/detail.rb
@@ -1,6 +1,6 @@
 class Detail < ApplicationRecord
   belongs_to :course
 
-  validates :course_name, presence: true
+  validates :course_name, presence: true, length: { minimum: 3 }
   validates :summary, presence: true, length: { minimum: 10 }
 end
--- spec/models/detail_spec.rb
+++ spec/models/detail_spec.rb
@@ -1,4 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe Detail, type: :model do
+  subject { Detail.new(course_name: 'A320 Cabin', summary:'123456') }
+
+  before { subject.save }
+
+  it 'course name should be present' do
+    subject.course_name = nil
+    expect(subject).to_not be_valid
+  end
+
+  it 'course name length should be >= 3' do
+    subject.course_name = 'A3'
+    expect(subject).to_not be_valid
+  end
+
+  it 'course summary should be present' do
+    subject.summary = nil
+    expect(subject).to_not be_valid
+  end
+
+  it 'course summary length should be >= 10' do
+    subject.summary = 'short sum'
+    expect(subject).to_not be_valid
+  end
 end
