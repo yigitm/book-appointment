a7ff149e0639e424fd654c59afc23f17141a4c4a
Yigit Mersin <yigitmersin@gmail.com>
2022-05-03 19:35:31 +0300

    Add course model test

--- spec/models/course_spec.rb
+++ spec/models/course_spec.rb
@@ -1,4 +1,32 @@
 require 'rails_helper'
 
 RSpec.describe Course, type: :model do
+  subject { Course.new(course_type: 'Pilot Courses', info:'Pilot course category') }
+
+  before { subject.save }
+
+  it 'course type should be present' do
+    subject.course_type = nil
+    expect(subject).to_not be_valid
+  end
+
+  it 'course type length should be >= 3' do
+    subject.course_type = 'Pc'
+    expect(subject).to_not be_valid
+  end
+
+  it 'course type should be unique & not be case sensitive' do
+    subject_two = Course.new(course_type: 'pilot courses', info:'pilot course category')
+    expect(subject_two).to_not be_valid
+  end
+
+  it 'course info should be present' do
+    subject.info = nil
+    expect(subject).to_not be_valid
+  end
+
+  it 'course type length should be >= 10' do
+    subject.info = 'Pc'
+    expect(subject).to_not be_valid
+  end
 end
