b19ca4d4267326eb9247d5e54c1339171e4498c9
Yigit Mersin <yigitmersin@gmail.com>
2022-05-11 22:53:09 +0300

    Update swagger defult path

--- spec/swagger_helper.rb
+++ spec/swagger_helper.rb
@@ -27,7 +27,7 @@ RSpec.configure do |config|
           url: 'http://{defaultHost}',
           variables: {
             defaultHost: {
-              default: 'localhost:3000/'
+              default: 'https://afternoon-ravine-99760.herokuapp.com/'
             }
           }
         }
