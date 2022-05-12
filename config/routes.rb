cbd58e4d0a49b02af12d87606dfede55bf0ddb73
Yigit Mersin <yigitmersin@gmail.com>
2022-05-12 04:04:54 +0300

    Remove root

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,6 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root 'users#index'
   namespace :api, default: { format: 'json' } do
     namespace :v1 do
       resources :users, only: %i[index create]
